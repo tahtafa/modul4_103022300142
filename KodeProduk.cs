718f426db968f2c80253c207e7690db3f91836fd
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-03-12 16:25:34 +0700

    menambahkan

---
+++ KodeProduk.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul4_103022300142
+{
+    internal class KodeProduk
+    {
+        private static Dictionary<string, string> kodeProdukTable = new Dictionary<string, string> 
+        {
+            {"Laptop", "E100"}, {"Smartphone", "E101"}, {"Tablet", "E102"},
+            {"Headset", "E103"}, {"Keyboard", "E104"}, {"Mouse", "E105"},
+            {"Printer", "E106"}, {"Monitor", "E107"}, {"Smartwatch", "E108"},
+            {"Kamera", "E109"}
+
+        };
+        public static string GetKodeProduk(string namaProduk)
+        {
+            return kodeProdukTable.ContainsKey(namaProduk) ? kodeProdukTable[namaProduk] : "Produk tidak ditemukan vro";
+
+        }
+
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using modul4_103022300142;
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Masukkan nama produk: ");
+        string namaProduk = Console.ReadLine();
+        string kodeProduk = KodeProduk.GetKodeProduk(namaProduk);
+        Console.WriteLine(kodeProduk);
+    }
+}
